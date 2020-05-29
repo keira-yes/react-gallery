c0dec6f1eaddd8057e928db16c341811b99a3b20
Kirillova <keira.kirillova@gmail.com>
2020-05-29 17:39:13 +0300

    add favicon, fonts, logo, main styles

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '../img/logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <h1><img src={logo} alt="Logo"/>Portfolio City</h1>
+        Integer dignissim tellus lacus, ut dapibus felis feugiat a. Aenean iaculis enim eros, vitae ultrices ligula semper ut. Quisque tempus dapibus consectetur. Nulla pretium nibh id arcu posuere tempor.
+      </div>
+    </div>
+  );
+}
+
+export default App;
