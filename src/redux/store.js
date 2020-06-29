1f24ccade1effac4774c964d67e7c90213994aa8
Kirillova <keira.kirillova@gmail.com>
2020-06-29 19:35:12 +0300

    add redux-thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 import rootReducer from "./rootReducer";
-
-const thunk = ({dispatch, getState}) => next => action => {
-  if (typeof action === "function") {
-    return action(dispatch, getState);
-  }
-  return next(action);
-};
 
 let store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 export default store;
