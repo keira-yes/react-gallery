15366d3ab6d65732592395fedc2fab126f8b6111
Kirillova <keira.kirillova@gmail.com>
2020-07-06 15:25:36 +0300

    fix method of fetch album photos

--- src/components/Album/Album.jsx
+++ src/components/Album/Album.jsx
@@ -6,36 +6,67 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import cover from './../../img/cover.jpg';
 import classes from './Album.module.css';
-import {useData} from '../../hoc/useData';
 
-const Album = ({album, albumsPhotos, errorAlbumsPhotos, loading}) => {
-  return (
-    <Link to={`/${album.userId}/album_${album.id}`} className={classes.Album}>
-      <Card className={classes.Card}>
-        <CardMedia
-          image={cover}
-          title="Contemplative Reptile"
-          className={classes.Image}
-        />
-        <CardContent>
-          <h3 className={classes.Title}>{album.title}</h3>
-          <p className={classes.Qty}>Photos:
-            {loading ? <span>Loading...</span> :
-              <span className={classes.Number}>{albumsPhotos.filter(item => Number(album.id) === Number(item.albumId)).length}</span>
-            }
-            {errorAlbumsPhotos && {errorAlbumsPhotos}}
-          </p>
-        </CardContent>
-      </Card>
-    </Link>
-  )
-};
+class Album extends React.Component {
 
-export default useData(Album);
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      albumsPhotos: [],
+      errorAlbumsPhotos: '',
+      loading: false
+    }
+  }
+
+  fetchAlbumsPhotos = () => {
+    this.setState({loading: true});
+    fetch(`https://jsonplaceholder.typicode.com/albums/${this.props.album.id}/photos`)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        else {
+          throw new Error(response.statusText)
+        }
+      })
+      .then(data => this.setState({albumsPhotos: data, loading: false}))
+      .catch(error => this.setState({errorAlbumsPhotos: error, loading: false}))
+  };
+
+  componentDidMount() {
+    this.fetchAlbumsPhotos();
+  }
+
+  render() {
+    const {albumsPhotos, errorAlbumsPhotos, loading} = this.state;
+    const {album} = this.props;
+
+    return (
+      <Link to={`/${album.userId}/album_${album.id}`} className={classes.Album}>
+        <Card className={classes.Card}>
+          <CardMedia
+            image={cover}
+            title="Contemplative Reptile"
+            className={classes.Image}
+          />
+          <CardContent>
+            <h3 className={classes.Title}>{album.title}</h3>
+            <p className={classes.Qty}>Photos:
+              {loading ? <span>Loading...</span> :
+                <span className={classes.Number}>{albumsPhotos.length}</span>
+              }
+              {errorAlbumsPhotos && errorAlbumsPhotos}
+            </p>
+          </CardContent>
+        </Card>
+      </Link>
+    )
+  }
+}
+
+export default Album;
 
 Album.propTypes = {
-  album: PropTypes.object.isRequired,
-  albumsPhotos: PropTypes.array.isRequired,
-  errorAlbumsPhotos: PropTypes.string,
-  loading: PropTypes.bool
+  album: PropTypes.object.isRequired
 };
--- src/components/Albums/Albums.jsx
+++ src/components/Albums/Albums.jsx
@@ -5,41 +5,15 @@ import classes from './Albums.module.css';
 import {useData} from '../../hoc/useData';
 
 class Albums extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      albumsPhotos: [],
-      errorAlbumsPhotos: '',
-      loading: false
-    }
-  }
-
-  fetchAlbumsPhotos = () => {
-    this.setState({loading: true});
-    fetch('https://jsonplaceholder.typicode.com/photos')
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        }
-        else {
-          throw new Error(response.statusText)
-        }
-      })
-      .then(data => this.setState({albumsPhotos: data, loading: false}))
-      .catch(error => this.setState({errorAlbumsPhotos: error}))
-  };
 
   componentDidMount() {
     const {user} = this.props.match.params;
     this.props.dataActions.fetchAlbums(user);
-    this.fetchAlbumsPhotos();
   }
 
   render() {
     const {user} = this.props.match.params;
     const {data, dataActions} = this.props;
-    const {albumsPhotos, errorAlbumsPhotos, loading} = this.state;
 
     return (
       <div className={classes.Albums}>
@@ -47,14 +21,7 @@ class Albums extends React.Component {
         <button type="button" className="back-btn" onClick={() => this.props.history.goBack()}>Back to Residents</button>
         {data.isLoading ? <p>Loading...</p> :
           <Grid container spacing={3}>
-            {data.albums.map(item => <Grid item xs={3} key={item.id}>
-              <Album
-                album={item}
-                albumsPhotos={albumsPhotos}
-                errorAlbumsPhotos={errorAlbumsPhotos}
-                loading={loading}
-              />
-            </Grid>)}
+            {data.albums.map(item => <Grid item xs={3} key={item.id}><Album album={item}/></Grid>)}
           </Grid>
         }
         {data.errorDataMessage && <p>Something went wrong. {data.errorDataMessage}. <button onClick={() => dataActions.fetchAlbums(user)}>Please try again!</button></p>}
